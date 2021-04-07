8f56fd1380697da3f006d5c73cea179d74ff131e
Marat <maratnazirov87@gmail.com>
2021-04-07 18:49:50 +0300

    Layout of elements on the screen

---
+++ GroceryApp/ViewControllers/ProductsTableViewController.swift
@@ -0,0 +1,32 @@
+//
+//  ProductsTableViewController.swift
+//  GroceryApp
+//
+//  Created by Marat on 07.04.2021.
+//
+
+import UIKit
+
+class ProductsTableViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    // MARK: - Table view data source
+
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        return 0
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 0
+    }
+
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+
+        return cell
+    }
+}
